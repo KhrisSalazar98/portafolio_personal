169890dfd5a5412b49896fce08c6f3e39bff4d68
cristobal <cristobal@jotaa.cl>
2023-07-26 01:58:19 -0400

    portafolio actualizado

--- src/components/ContenidoHabilidades.jsx
+++ src/components/ContenidoHabilidades.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-
-import htmlImg from '../img/html.png';
-import cssImg from '../img/css.png';
-import js from '../img/js.png';
-import bootstrapImg from '../img/bootstrap.png';
-import sass from '../img/sass.png';
-import reactImg from '../img/react.png';
-import npm from '../img/npm.png';
-import reduxToolKitImg from '../img/redux_toolkit.png';
-import figma from '../img/figma.png';
+import {listConocimientos} from '../js/data'; 
 
 const ContenidoHabilidades = () => {
 
@@ -19,33 +10,19 @@ const ContenidoHabilidades = () => {
             </h2>
             <hr />
 
-            <div className='mt-5'>
-                <ul>
-                    <li className='text-start mb-1'><span className='html'>HTML</span></li>
-                    <li className='text-start mb-1'><span className='css'>CSS</span></li>
-                    <li className='text-start mb-1'><span className='js'>JavaScript</span></li>
-                    <li className='text-start mb-1'><span className='bootstrap'>Framework de CSS "Bootstrap"</span></li>
-                    <li className='text-start mb-1'><span className='react'>Librería de JavaScript "React"</span></li>
-                    <li className='text-start mb-1'><span className='sass'>Preprocesador de CSS "SASS"</span></li>
-                    <li className='text-start mb-1'><span className='reduxToolkit'>Redux Toolkit</span></li>
-                    <li className='text-start mb-1'><span className='npm'>Gestor de paquetes "NPM" (básico)</span></li>
-                    <li className='text-start mb-1'><span className='git'>Controlador de versiones Git</span></li>
-                    <li className='text-start mb-1'><span className='figma'>Figma</span></li>
-                </ul>
-            </div>
+            <div className="row mt-5 pt-5">
+                
 
-            <div className='d-flex justify-content-center align-items-center mb-5'>
-                <div className="box">
-                    <span className='img_html'><img src={htmlImg} alt="HTML" /></span>
-                    <span className='img_css'><img src={cssImg} alt="CSS" /></span>
-                    <span className='img_bootstrap'><img src={bootstrapImg} alt="Boostrap" /></span>
-                    <span className='img_js'><img src={js} alt="JavaScript" /></span>
-                    <span className='img_react'><img src={reactImg} alt="React" /></span>
-                    <span className='img_sass'><img src={sass} alt="SASS" /></span>
-                    <span className='img_npm'><img src={npm} alt="NPM" /></span>
-                    <span className='img_reduxToolkit'><img src={reduxToolKitImg} alt="Redux Toolkit" /></span>
-                    <span className='img_figma'><img src={figma} alt="Figma" /></span>                
-                </div>
+                {listConocimientos.map((item,index) =>(
+                    <div key={index} className='col-6 col-sm-4 col-lg-2 mb-3'>
+                        <div className='m-2'>
+                            <img className='img_habilidad' src={item.img} alt={item.name} />
+                            <p className={`mt-3 txt_habilidad ${item.class}`}>{item.name}</p>
+                        </div>
+                    </div>
+                ))}
+                
+
             </div>
         </div>
     )
